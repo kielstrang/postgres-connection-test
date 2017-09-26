70bd4be39d54eae97bdf73a605708eeed85482ef
vagrant <kiel.strang@gmail.com>
2017-09-26 18:29:47 +0000

    Refactoring

--- find_people.js
+++ find_people.js
@@ -12,19 +12,33 @@ const client = new pg.Client({
 
 const name = process.argv[2];
 
+function getPeopleByName(client) {
+  return (name, cb) => {
+    client.query('SELECT * FROM famous_people WHERE (first_name=$1) OR (last_name=$1)', [name], (err, result) => {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      let people = [];
+      result.rows.forEach(person => {
+        person.birthdate = new Date(person.birthdate).toISOString().split('T')[0];
+        people.push(person);
+      });
+      cb(name, people);
+    });
+  };
+}
+
+function printPeopleByName(name, people) {
+  console.log(`Found ${people.length} person(s) by the name '${name}'`);
+  people.forEach((person) => {
+    console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`);
+  });
+  client.end();
+}
+
 client.connect((err) => {
   if(err) {
     return console.error('Connection Error', err);
   }
-  client.query('SELECT * FROM famous_people WHERE (first_name=$1) OR (last_name=$1)', [name], (err, result) => {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(`Found ${result.rows.length} person(s) by the name '${name}`);
-    result.rows.forEach((person) => {
-      const date = new Date(person.birthdate).toISOString().split('T')[0];
-      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born ${date}`);
-    });
-    client.end();
-  });
+  getPeopleByName(client)(name, printPeopleByName);
 });
